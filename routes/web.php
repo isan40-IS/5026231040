bb500ec5f06ebabb43fec802fa69193109da6feb
Hizkia Crisantino <hizkiacrisantino98@gmail.com>
2025-05-14 09:23:33 +0700

    Pertemuan_11

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Coba;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('hello', [Coba::class, 'helloWorld']);
